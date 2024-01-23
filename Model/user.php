c5ec90363d7f6a2d0fed8993c2791f17302865a6
ThangVo0308 <thangvo0308@gmail.com>
2024-01-23 14:40:02 +0700

    .

---
+++ Model/user.php
@@ -0,0 +1,19 @@
+<?php
+    class products {
+        private $id,$name,$category_id,$image,$gender,$price,$description,$status;
+
+        public function __construct($id,$name,$category_id,$image,$gender,$price,$description,$status) {
+            $this->id= $id;
+            $this->name=$name;
+            $this->category_id=$category_id;
+            $this->image=$image;
+            $this->gender=$gender;
+            $this->price=$price;
+            $this->description=$description;
+            $this->status=$status;
+        }
+
+        
+    }
+
+?>
