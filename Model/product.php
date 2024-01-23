7dc2b53a224c807ffc4fca4a06e22649422eea47
ThangVo0308 <thangvo0308@gmail.com>
2024-01-23 14:59:33 +0700

    add product model

--- Model/product.php
+++ Model/product.php
@@ -13,7 +13,69 @@
             $this->status=$status;
         }
 
+        public function setID($id) {
+            $this->id = $id;
+          }
+          
+        public function getID() {
+            return $this->id;
+        }
 
+        public function setName($name) {
+            $this->name = $name;
+          }
+          
+        public function getName() {
+            return $this->name;
+        }
+
+        public function setCategoryID($category_id) {
+            $this->category_id = $category_id;
+          }
+          
+        public function getCaterogyID() {
+            return $this->category_id;
+        }
+
+        public function setImage($image) {
+            $this->image = $image;
+        }
+
+        public function getImage() {
+            return $this->image;
+        }
+
+        public function getPrice() {
+            return $this->price;
+        }
+
+        public function setPrice($price) {
+            $this->price = $price;
+        }
+
+        public function getDescription() {
+            return $this->description;
+        }
+
+        public function setDescription($description) {
+            $this->description = $description;
+        }
+
+        public function getStatus() {
+            return $this->status;
+        }
+
+        public function setStatus($status) {
+            $this->status = $status;
+        }
+
+        public function getGender() {
+            return $this->gender;
+        }
+
+        public function setGender($status) {
+            $this->status = $status;
+        }
     }
 
 ?>
