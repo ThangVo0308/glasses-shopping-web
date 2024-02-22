0dc4347d946a8585076d1b448b1000e259bbd438
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-02-22 21:20:12 +0700

    Update database.php

--- BE/DAL/database.php
+++ BE/DAL/database.php
@@ -7,7 +7,6 @@
         try{
             $connection = new PDO("mysql:host=".DATABASE_SERVER.";dbname=".DATABASE_NAME,DATABASE_USER,DATABASE_PASSWORD);
             $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            echo "Connect sucessfully";
         }catch(PDOException $e) {
             echo "Connection failed: ".$e->getMessage();
             $connection = null;
