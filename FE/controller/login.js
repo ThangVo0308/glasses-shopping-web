d1b0d220de8d2379e51133ad4d6a19790277c454
qcuong57 <hoquoccuong945@gmail.com>
2024-02-25 15:36:14 +0700

    add css Validate Login

--- FE/controller/login.js
+++ FE/controller/login.js
@@ -1,6 +1,16 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const form = document.getElementById('form');
+
+const usernameLogin = document.getElementById('username');
+const passwordLogin = document.getElementById('password');
+const usernameSignUp= document.getElementById('usernameSignUp');
+const passwordSignUp = document.getElementById('passwordSignUp');
+const nameSignUp = document.getElementById('name');
+const emailSignUp = document.getElementById('email');
+const phoneSignUp = document.getElementById('phone');
+const addressSignUp = document.getElementById('address');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -25,10 +35,99 @@ eyeClose.addEventListener("click", function() {
     input.setAttribute("type","text");    
 });
 
-function changeLogin() {
-    var containerMove= document.querySelector(".container-move");
-    container.classList.add("show-form")
-    console.log(container.style);
-    console.log('Login image clicked');
+container.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    validateInput();
+})
 
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    
+    errorDisplay.innerText='';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase());
 }
+
+
+const validateInput = () => {
+    const usernameLoginValue = usernameLogin.value.trim();
+    const passwordLoginValue = passwordLogin.value.trim();
+    const usernameSignUpValue = usernameSignUp.value.trim();
+    const passwordSignUpValue = passwordSignUp.value.trim();
+    const nameValue = nameSignUp.value.trim();
+    const emailValue = emailSignUp.value.trim();
+    const phoneValue = phoneSignUp.value.trim();
+    const addressValue = addressSignUp.value.trim();
+    
+    if (usernameLoginValue === '') {
+        setError(usernameLogin, 'Username is required');
+    } else {
+        setSuccess(usernameLogin);
+    }
+    
+    if (passwordLoginValue === '') {
+        setError(passwordLogin, 'Password is required');
+    } else if (passwordLoginValue.length < 8) {
+        setError(passwordLogin, 'Password must be at least 8 characters');
+    } else {
+        setSuccess(passwordLogin);
+    }
+
+    if(usernameSignUpValue === ''){
+        setError(usernameSignUp,'Username is required');
+    }else{
+        setSuccess(usernameSignUp);
+    }
+
+    if (passwordSignUpValue === '') {
+        setError(passwordSignUp, 'Password is required');
+    } else if (passwordSignUpValue.length < 8) {
+        setError(passwordSignUp, 'Password must be at least 8 characters');
+    } else {
+        setSuccess(passwordSignUp);
+    }
+    
+    if (nameValue === '') {
+        setError(nameSignUp, 'Name is required');
+    } else {
+        setSuccess(nameSignUp);
+    }
+
+    if (emailValue === '') {
+        setError(emailSignUp, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(emailSignUp, 'Provide a valid email address');
+    }
+     else {
+        setSuccess(emailSignUp);
+    }
+
+    if (phoneValue === '') {
+        setError(phoneSignUp, 'Phone is required');
+    } else {
+        setSuccess(phoneSignUp);
+    }
+
+    if(addressValue === ''){
+        setError(addressSignUp,'Address is required');
+    }else {
+        setSuccess(addressSignUp)
+    }
+};
+
--- FE/html/login.php
+++ FE/html/login.php
@@ -4,31 +4,55 @@
             <div class="form-container sign-up">
                 <form id="form">
                     <h1>Đăng ký</h1>
-                    <input type="text" placeholder="Username" class="txtUsernameSignUp" id="input-box">
-                    <input type="password" placeholder="Password" class="txtPasswordSignUp" id="input-box">
-                    <input type="text" placeholder="Họ Tên" class="txtName" id="input-box">
-                    <input type="email" placeholder="Email" id="txtEmail" id="input-box">
-                    <input type="text" placeholder="Số điện thoại" class="txtPhone" id="input-box">
-                    <input type="text" placeholder="Địa chỉ" class="txtAddress" id="input-box">
-                    <button class="button">Đăng ký</button>
+                    <div class="input-control">
+                        <input type="text" placeholder="Username" class="txtUsernameSignUp" id="input-box">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control">
+                        <input type="password" placeholder="Password" class="txtPasswordSignUp" id="input-box">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control">
+                        <input type="text" placeholder="Họ Tên" class="txtName" id="input-box">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control">
+                        <input type="email" placeholder="Email" id="txtEmail" id="input-box">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control">
+                        <input type="text" placeholder="Số điện thoại" class="txtPhone" id="input-box">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control">
+                        <input type="text" placeholder="Địa chỉ" class="txtAddress" id="input-box">
+                        <div class="error"></div>
+                    </div>
+                    <button class="button" type="submit">Đăng ký</button>
                 </form>
             </div>
             <div class="form-container sign-in">
                 <form id="form">
                     <h1>Đăng nhập</h1>
-                    <input type="text" placeholder="Username" id="input-box" class="txtUsernameLogin">
-                    <input type="password" placeholder="Password" id="input-box" class="txtPasswordLogin">
+                    <div class= "input-control">
+                        <input type="text" placeholder="Username" id="input-box" class="txtUsernameLogin">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control">
+                        <input type="password" placeholder="Password" id="input-box" class="txtPasswordLogin">
+                        <div class="error"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="eye eye-close">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </svg>
+    
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="eye eye-open hide">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                    </div>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="eye eye-close">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                    </svg>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="eye eye-open hide">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
-
-                    <button class="button">Đăng nhập</button>
+                    <button class="button" type="submit">Đăng nhập</button>
                 </form>
             </div>
             <div class="toggle-container">
