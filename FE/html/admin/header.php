8a37f5cec04ff24c057d45645fcd270d9e7b649a
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-02-26 20:57:02 +0700

    Use an Array instead of a database

--- FE/html/admin/header.php
+++ FE/html/admin/header.php
@@ -30,26 +30,41 @@ $function = getFunction();
     </div>
 </div>
 <?php
-function getFunction()
-{
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'web2';
+// Dùng database
+// function getFunction()
+// {
+//     $servername = 'localhost';
+//     $username = 'root';
+//     $password = '';
+//     $dbname = 'web2';
 
-    try {
-        $dp = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $dp->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-    $sql = "SELECT tenChucNang, icon FROM chucnang ORDER BY maChucNang;";
-    $stmt = $dp->prepare($sql);
-    $stmt->execute();
-    $functions= array();
+//     try {
+//         $dp = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//         $dp->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     } catch (PDOException $e) {
+//         echo "Connection failed: " . $e->getMessage();
+//     }
+//     $sql = "SELECT tenChucNang, icon FROM chucnang ORDER BY maChucNang;";
+//     $stmt = $dp->prepare($sql);
+//     $stmt->execute();
+//     $functions= array();
     
-        while($row= $stmt->fetch(PDO::FETCH_ASSOC)){
-            array_push($functions,$row);
-        }
+//         while($row= $stmt->fetch(PDO::FETCH_ASSOC)){
+//             array_push($functions,$row);
+//         }
+//     return $functions;
+// }
+// Dùng array
+function getFunction()
+ {
+    $functions = [
+        ['tenChucNang' => 'Thống kê', 'icon' => 'fa-solid fa-chart-column'],
+        ['tenChucNang' => 'Sản phẩm', 'icon' => 'fa-solid fa-warehouse'],
+        ['tenChucNang' => 'Đơn hàng', 'icon' => 'fa-solid fa-cart-shopping'],
+        ['tenChucNang' => 'Tài khoản', 'icon' => 'fa-regular fa-user'],
+        ['tenChucNang' => 'Nhập hàng', 'icon' => 'fa-solid fa-plus'],
+        ['tenChucNang' => 'Giao diện', 'icon' => 'fa-regular fa-image'],
+        ['tenChucNang' => 'Phân quyền', 'icon' => 'fa-solid fa-user-pen'],
+    ];
     return $functions;
-}
+ }
