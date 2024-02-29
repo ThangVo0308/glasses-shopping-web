b6d3e47cdb2e7b234518837cff266d14d41d3d8c
Trieu Den <yantic088@gmail.com>
2024-02-29 22:19:54 +0700

    product detail demo

--- FE/html/product/productDetail.php
+++ FE/html/product/productDetail.php
@@ -4,16 +4,27 @@
 ?>
 
 <div id="product-detail">
-    <img src="../../../images/logo.png" alt="">
+    <img src="../../../images/products/productDemo.png" alt="">
     <div id="infor">
         <h3 id="name-product"><?php echo $product['name']; ?></h3>
+        <span id="id-product" >Mã sản phẩm: NBA-2001</span>
         <div>
-            <span id="price">100.000</span>
-            <span id="price-real">500.000</span>
+            <span id="price">100.000 đ</span>
+            <span id="price-real">500.000 đ</span>
         </div>
-        <span>Trạng thái: còn hàng</span>
-        <span>Giới tính: none</span>
+        <span id="discout-time" >Giá tiền được áp dụng từ ngày 20/2 đến 22/2</span>
         <textarea name="" id="" cols="30" rows="10">Chất lượng đỉnh cao</textarea>
+        <div class="quantity-form" >
+            <button id="decrease">-</button>
+            <input id="quantity" type="text" value="5">
+            <button id="increase">+</button>
+            <img src="../../../icons/tick.png" alt="">
+            <span class="status" >Còn 5 sản phẩm</span>
+        </div>
+        <div>
+            <button>Hủy</button>
+            <button>Thêm vào giỏ hàng</button>
+        </div>
     </div>
     <link rel="stylesheet" href="../../css/productStyle/productDetail.css">
 </div>
