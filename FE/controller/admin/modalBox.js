6d0bc2426990b14df14c3f8341d1bfb4186b1d0b
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-03-05 14:18:44 +0700

    Add files via upload

---
+++ FE/controller/admin/modalBox.js
@@ -0,0 +1,16 @@
+function openNewproduct(){
+    let newProduct = document.querySelector("#new-product");
+    newProduct.style.display="block";
+}
+function closeEditproduct() {
+let editProduct = document.querySelector("#edit-product");
+editProduct.style.display = "none";
+}
+function closeDetailproduct() {
+let detailProduct = document.querySelector("#detail-product");
+detailProduct.style.display = "none";
+}
+function closeNewproduct() {
+let newProduct = document.querySelector("#new-product");
+newProduct.style.display = "none";
+}
