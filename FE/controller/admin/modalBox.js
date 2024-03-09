cbcf5f1a60f8060f5603a60f612daa9d26fd5087
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-03-09 17:01:00 +0700

    add some stuff

--- FE/controller/admin/modalBox.js
+++ FE/controller/admin/modalBox.js
@@ -2,6 +2,10 @@ function openNewproduct(){
     let newProduct = document.querySelector("#new-product");
     newProduct.style.display="block";
 }
+function openNewaccount(){
+    let newAccount = document.querySelector("#new-account");
+    newAccount.style.display="block";
+}
 function closeEditproduct() {
 let editProduct = document.querySelector("#edit-product");
 editProduct.style.display = "none";
@@ -10,7 +14,16 @@ function closeDetailproduct() {
 let detailProduct = document.querySelector("#detail-product");
 detailProduct.style.display = "none";
 }
-function closeNewproduct() {
-let newProduct = document.querySelector("#new-product");
-newProduct.style.display = "none";
+function closeDetailorder() {
+    let detailOrder = document.querySelector("#detail-order");
+    detailOrder.style.display = "none";
 }
+function closeDetailaccount() {
+    let detailAccount = document.querySelector("#detail-account");
+    detailAccount.style.display = "none";
+}
+
+function closeNewaccount() {
+    let newAccount = document.querySelector("#new-account");
+    newAccount.style.display = "none";
+    }
