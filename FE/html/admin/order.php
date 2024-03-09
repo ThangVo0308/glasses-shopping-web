0196c228f5bf0126c38c09460517ae74b498c51f
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-03-09 17:03:46 +0700

    Add button and search bar

--- FE/html/admin/order.php
+++ FE/html/admin/order.php
@@ -6,6 +6,7 @@ $order=[
         'name'=> 'Nguyen van A',
         'date'=>'27/02/2024',
         'total'=>100000,
+        'discount'=>0,
         'status'=>'Shipped'
     ],
     [
@@ -13,6 +14,7 @@ $order=[
         'name'=> 'Nguyen van B',
         'date'=>'24/02/2024',
         'total'=>50000,
+        'discount'=>0,
         'status'=>'Pending'
     ],
     ];
@@ -20,12 +22,35 @@ $order=[
 <div id="order">
     <div class="header">
     <h2><i class="fa-solid fa-cart-shopping"></i><span> Đơn hàng </span></h2>
+    <div class="search-bar">
+            <div class="search-input">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text" placeholder="Bạn muốn tìm gì?">
+            </div>
+            <div class="filter-input">
+                <i class="fa-solid fa-filter"></i>
+                <select name="" id="" >
+                    <option value="All">All</option>
+                    <option value="Pending">Pending</option>
+                    <option value="Shipping">Shipping</option>
+                    <option value="Delivered">Delivered</option>
+                    <option value="Cancel">Cancel</option>
+                </select>
+            </div>
+            <div class="date-begin">
+                <input type="date" name="" id="" >
+            </div>
+            <div class="date-end">
+                <input type="date" name="" id="" >
+            </div>
+        </div>
     </div>
     <div class="title-list">
         <div class="title-placeholder">
             <div class="title">Mã đơn hàng</div>
             <div class="title">Tên khách hàng</div>
             <div class="title">Ngày mua hàng</div>
+            <div class="title">Giảm giá</div>
             <div class="title">Tổng tiền</div>
             <div class="title">Trạng thái</div>
         </div>
@@ -45,16 +70,21 @@ $order=[
                             <?=$order[$i]['date']?>
                         </div>
                         <div class="item">
+                            <?=$order[$i]['discount']?>
+                        </div>
+                        <div class="item">
                             <?=$order[$i]['total']?>
                         </div>
                         <div class="item">
                             <?=$order[$i]['status']?>
                         </div>
-                        <div class="item">
+                        <div class="item" onclick="loadModalBoxByAjax('detailOrder',<?=$i?>)">
                             <i class="fa-solid fa-circle-info"></i>
                         </div>
                     </div>
                 </div>
                 <?php endfor; ?>
             </div>
+            <div id="modal-box"></div>
+    </div>
     </div>
