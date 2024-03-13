b55264dfe8f31a7382495ecc4b37d2ffa63e8d14
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-03-13 22:01:22 +0700

    Update and rename interface.php to discount.php

---
+++ FE/html/admin/discount.php
@@ -0,0 +1,47 @@
+<div id="discount">
+    <div class="header">
+        <h2><i class="fa-solid fa-percent"></i> Giảm giá</h2>
+        <div class="button-placeholder" onclick="loadModalBoxByAjax('newDiscount')">
+        <div class="new-button">
+        <div class="icon-placeholder"><i class="fa-solid fa-user-plus fa-sm"></i></div>
+        <div class="info-placeholder">Thêm</div>
+        </div>
+        </div>
+        </div>
+        <div class="title-list">
+            <div class="title-placeholder">
+                <div class="title">Mã giảm giá</div>
+                <div class="title">Tên chương trình</div>
+                <div class="title">% giảm giá</div>
+                <div class="title">Ngày bắt đầu</div>
+                <div class="title">Ngày kết thúc</div>
+            </div>
+        </div>
+        <div class="list">
+        <div class="placeholder">
+        <div class="info">
+            <div class="item">
+                1
+            </div>
+            <div class="item">
+                Khai Xuân
+            </div>
+            <div class="item">
+                15
+            </div>
+            <div class="item">
+                01/01/2024
+            </div>
+            <div class="item">
+                31/03/2024
+            </div>
+            <div class="item" onclick="loadModalBoxByAjax('detailDiscount')">
+            <i class="fa-solid fa-circle-info" ></i>
+            </div>
+        </div>
+        </div>
+        </div>
+        <div id="modal-box"></div></div>
+    
+
+
--- FE/html/admin/interface.php
+++
@@ -1,2 +0,0 @@
-<div id="interface">
-    <h2><i class="fa-regular fa-image"></i><span> Giao diện </span></h2>
