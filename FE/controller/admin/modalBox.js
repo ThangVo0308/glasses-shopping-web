b42c2ac40c5b172980785b38833f772fef9cead3
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-03-13 23:05:38 +0700

    fix some stuff

--- FE/controller/admin/modalBox.js
+++ FE/controller/admin/modalBox.js
@@ -2,6 +2,10 @@ function openNewproduct(){
     let newProduct = document.querySelector("#new-product");
     newProduct.style.display="block";
 }
+function closeNewproduct(){
+    let newProduct = document.querySelector("#new-product");
+    newProduct.style.display="none";
+}
 function openNewaccount(){
     let newAccount = document.querySelector("#new-account");
     newAccount.style.display="block";
@@ -22,8 +26,43 @@ function closeDetailaccount() {
     let detailAccount = document.querySelector("#detail-account");
     detailAccount.style.display = "none";
 }
+function closeDetaildiscount() {
+    let detaildiscount = document.querySelector("#detail-discount");
+    detaildiscount.style.display = "none";
+}
+function closeEditdiscount() {
+    let detaildiscount = document.querySelector("#edit-discount");
+    detaildiscount.style.display = "none";
+}
+function closeNewdiscount() {
+    let newdiscount = document.querySelector("#new-discount");
+    newdiscount.style.display = "none";
+}
+function closeDetailpoint(){
+    let detailpoint = document.querySelector("#detail-point");
+    detailpoint.style.display = "none";
+}
 
+function openNewsupply(){
+    let newsupply = document.querySelector("#new-supply");
+    newsupply.style.display="block";
+}
+function closeDetailsupply(){
+    let detailsupply = document.querySelector("#detail-supply");
+    detailsupply.style.display = "none";
+}
 function closeNewaccount() {
     let newAccount = document.querySelector("#new-account");
     newAccount.style.display = "none";
-    }
+}
+function closeNewsupply() {
+    let newsupply = document.querySelector("#new-supply");
+    newsupply.style.display = "none";
+}
+
+const openAddproduct = () => {
+    document.querySelector("#add_exist_product").style.display = "block";
+    };
+const closeAddproduct = () => {
+document.querySelector("#add_exist_product").style.display = "none";
+};
