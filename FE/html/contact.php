dcfd33a2a352b29d91f4c0c36fd432a963d17dce
Trieu Den <yantic088@gmail.com>
2024-03-15 23:00:31 +0700

    contact email

--- FE/html/contact.php
+++ FE/html/contact.php
@@ -1,7 +1,7 @@
 <body>
     <iframe src="./path.php" frameborder="0" id="pathContact"></iframe>
-    <div id="contact" >
-        <div id="information" >
+    <div id="contact">
+        <div id="information">
             <div class="section">
                 <img src="../../icons/location.png" alt="">
                 <div>
@@ -24,13 +24,48 @@
                 </div>
             </div>
         </div>
-        <div id="emailContact" >
+        <div id="emailContact">
             <span>Mọi vấn đề về cửa hàng được giải quyết tại đây</span>
-            <input type="text" name="" id="" placeholder="Họ và tên"  >
-            <input type="email" name="" id="" placeholder="Email">
-            <textarea name="note" id="" placeholder="Ghi chú" rows="4" ></textarea>
-            <button>Xác Nhận</button>
+            <input type="text" name="" id="nameUser" placeholder="Họ và tên">
+            <input type="email" name="" id="emailUser" placeholder="Email">
+            <textarea name="note" id="noteValue" placeholder="Ghi chú" rows="4"></textarea>
+            <button onclick="sendEmail()">Xác Nhận</button>
         </div>
     </div>
 </body>
 <link rel="stylesheet" href="../css/contact.css">
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script>
+    function sendEmail() {
+        var nameUser = document.getElementById('nameUser').value;
+        var emailUser = document.getElementById('emailUser').value;
+        var noteValue = document.getElementById('noteValue').value;
+        if (nameUser.trim() === "" || emailUser.trim() === "" || noteValue.trim() === "") {
+            alert('Vui lòng điền đầy đủ thông tin')
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "../../main/handler/sendEmail.php",
+                dataType: 'json',
+                data: {
+                    nameUser: nameUser,
+                    emailUser: emailUser,
+                    noteValue: noteValue,
+                },
+                beforeSend: function() {
+                    alert("Vui lòng chờ...");
+                },
+                success: function(response) {
+                    if (response.success) {
+                        alert('Phản hòi thành công')
+                    } else {
+                        console.log("Đã có lỗi");
+                    }
+                },
+                error: function() {
+                    console.log("Có lỗi khi gửi yêu cầu đến máy chủ");
+                }
+            });
+        }
+    }
+</script>
