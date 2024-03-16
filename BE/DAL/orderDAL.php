84a846907b16dac6e919fb8cac691ccbf79fa2ad
ThangVo0308 <thangvo0308@gmail.com>
2024-03-16 16:32:31 +0700

    .

--- BE/DAL/orderDAL.php
+++ BE/DAL/orderDAL.php
@@ -19,7 +19,10 @@
         $order->setUserid($row['user_id']);
         $order->setOrderdate($row['order_date']);
         $order->setTotalprice($row['total_price']);
-
+        $order->setTotalprice($row['points_earned']);
+        $order->setTotalprice($row['points_used']);
+        $order->setTotalprice($row['address_id']);
+        $order->setTotalprice($row['status']);
 
         return $order;
     }
@@ -49,22 +52,19 @@
                 $total_price = $order->getTotalprice();
                 $points_earned = $order->getPointsearned();
                 $points_used = $order->getPointused();
-                $address = $order->getAddress();
-                $name_received = $order->getNamereceived();
-                $phone_received = $order->getPhonereceived();
+                $address_id = $order->getAddressId();
                 $status = $order->getStatus();
         
-                $query = "INSERT INTO orders (user_id, order_date, total_price, points_earned, points_used, address, name_received, phone_received, status) 
-                          VALUES (:user_id, :order_date, :total_price, :points_earned, :points_used, :address, :name_received, :phone_received, :status)";
+                $query = "INSERT INTO orders (user_id, order_date, total_price, points_earned, points_used, address_id, status) 
+                          VALUES (:user_id, :order_date, :total_price, :points_earned, :points_used, :address_id, :status)";
                 $statement = $this->connection->prepare($query);
                 $statement->bindParam(':user_id', $user_id);
                 $statement->bindParam(':order_date', $order_date);
                 $statement->bindParam(':total_price', $total_price);
                 $statement->bindParam(':points_earned', $points_earned);
                 $statement->bindParam(':points_used', $points_used);
-                $statement->bindParam(':address', $address);
-                $statement->bindParam(':name_received', $name_received);
-                $statement->bindParam(':phone_received', $phone_received);
+                $statement->bindParam(':address_id', $address_id);
+
                 $statement->bindParam(':status', $status);
                 $statement->execute();
                 return true;
@@ -78,18 +78,26 @@
 
         public function updateOrder(orders $order) {
             try {
-                $query = "update orders set user_id=?, order_date = ?, total_price=? where id=?";
+                $query = "update orders set user_id=?, order_date = ?, total_price=?, points_earned = ?,points_used = ?, address_id = ?,status = ? where id=?";
                 $statement = $this->connection->prepare($query);
 
                 $user_id = $order->getUserid();
                 $order_date = $order->getOrderdate();
-                $total_price = $order->getTotalprice();     
+                $total_price = $order->getTotalprice();   
+                $points_earned = $order->getPointsEarned();     
+                $points_used = $order->getPointUsed();   
+                $address_id = $order->getAddressId();     
+                $status = $order->getStatus();
                 $id = $order->getID();
 
                 $statement->bindParam(1,$user_id);
                 $statement->bindParam(2,$order_date);
                 $statement->bindParam(3,$total_price);
-                $statement->bindParam(4,$id);
+                $statement->bindParam(4,$points_earned);
+                $statement->bindParam(5,$points_used);
+                $statement->bindParam(6,$address_id);
+                $statement->bindParam(7,$status);
+                $statement->bindParam(8,$id);
 
                 $statement->execute();
                 return true;
@@ -118,13 +126,13 @@
             $orderList = array();
 
             if($condition == null || strlen($columnName) == 0) {
-                $query = "select * from orders where concat(id, user_id, order_date, total_price) like ?";
+                $query = "select * from orders where concat(id, user_id, order_date, total_price,points_earned, points_used, address_id,status) like ?";
             }else if(strlen($columnName) == 1) {
                 $column = $columnName[0]; // selected column
                 $query = "select * from orders where ".$column.+" like ?";
             }else {
                 $columns = implode(",",$columnName);
-                $query = "select id, user_id, order_date, total_price from orders where concat(".$columns.") like ?";
+                $query = "select id, user_id, order_date, total_price, points_earned, points_used, address_id, status from orders where concat(".$columns.") like ?";
             }
 
             $statement = $this->connection->prepare($query);
--- Model/orders.php
+++ Model/orders.php
@@ -1,16 +1,15 @@
 <?php
-    class orders{
-        private $id,$user_id,$order_date,$total_price,$points_earned,$point_used,$address,$name_received,$phone_received,$status;
-        public function __construct($id = null, $userid = null, $orderdate = null, $totalprice = null, $pointsearned = null, $pointused = null, $address = null, $namereceived = null, $phonereceived = null, $status = null) { 
+    class Orders {
+        private $id, $userId, $orderDate, $totalPrice, $pointsEarned, $pointUsed, $addressId, $status;
+        
+        public function __construct($id = null, $userId = null, $orderDate = null, $totalPrice = null, $pointsEarned = null, $pointUsed = null, $addressId = null, $status = null) { 
             $this->id = $id; 
-            $this->user_id = $userid; 
-            $this->order_date = $orderdate; 
-            $this->total_price = $totalprice; 
-            $this->points_earned = $pointsearned; 
-            $this->point_used = $pointused; 
-            $this->address = $address; 
-            $this->name_received = $namereceived; 
-            $this->phone_received = $phonereceived; 
+            $this->userId = $userId; 
+            $this->orderDate = $orderDate; 
+            $this->totalPrice = $totalPrice; 
+            $this->pointsEarned = $pointsEarned; 
+            $this->pointUsed = $pointUsed; 
+            $this->addressId = $addressId; 
             $this->status = $status; 
         }
         
@@ -18,36 +17,28 @@
              return $this->id; 
         } 
     
-        function getUserid() { 
-             return $this->user_id; 
+        function getUserId() { 
+             return $this->userId; 
         } 
     
-        function getOrderdate() { 
-             return $this->order_date; 
+        function getOrderDate() { 
+             return $this->orderDate; 
         } 
     
-        function getTotalprice() { 
-             return $this->total_price; 
+        function getTotalPrice() { 
+             return $this->totalPrice; 
         } 
     
-        function getPointsearned() { 
-             return $this->points_earned; 
+        function getPointsEarned() { 
+             return $this->pointsEarned; 
         } 
     
-        function getPointused() { 
-             return $this->point_used; 
+        function getPointUsed() { 
+             return $this->pointUsed; 
         } 
     
-        function getAddress() { 
-             return $this->address; 
-        } 
-    
-        function getNamereceived() { 
-             return $this->name_received; 
-        } 
-    
-        function getPhonereceived() { 
-             return $this->phone_received; 
+        function getAddressId() { 
+             return $this->addressId; 
         } 
     
         function getStatus() { 
@@ -58,41 +49,32 @@
             $this->id = $id; 
         } 
     
-        function setUserid($userid) {  
-            $this->user_id = $userid; 
+        function setUserId($userId) {  
+            $this->userId = $userId; 
         } 
     
-        function setOrderdate($orderdate) {  
-            $this->order_date = $orderdate; 
+        function setOrderDate($orderDate) {  
+            $this->orderDate = $orderDate; 
         } 
     
-        function setTotalprice($totalprice) {  
-            $this->total_price = $totalprice; 
+        function setTotalPrice($totalPrice) {  
+            $this->totalPrice = $totalPrice; 
         } 
     
-        function setPointsearned($pointsearned) {  
-            $this->points_earned = $pointsearned; 
+        function setPointsEarned($pointsEarned) {  
+            $this->pointsEarned = $pointsEarned; 
         } 
     
-        function setPointused($pointused) {  
-            $this->point_used = $pointused; 
+        function setPointUsed($pointUsed) {  
+            $this->pointUsed = $pointUsed; 
         } 
     
-        function setAddress($address) {  
-            $this->address = $address; 
-        } 
-    
-        function setNamereceived($namereceived) {  
-            $this->name_received = $namereceived; 
-        } 
-    
-        function setPhonereceived($phonereceived) {  
-            $this->phone_received = $phonereceived; 
+        function setAddressId($addressId) {  
+            $this->addressId = $addressId; 
         } 
     
         function setStatus($status) {  
             $this->status = $status; 
         } 
     }
-
 ?>
