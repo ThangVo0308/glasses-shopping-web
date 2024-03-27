c54c3e5ab5a24f54775dcbcd1bc54081313c55cf
XiuQuy <138677853+XiuQuy@users.noreply.github.com>
2024-03-27 23:34:42 +0700

    add admin.php

---
+++ admin/admin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel='shortcut icon' href='../images/logo_web.ico' />
+  <meta name="application-name" content="Administrator">
+    <!-- library -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@700;800&display=swap" rel="stylesheet">
+  <!-- css -->
+  <link rel="stylesheet" href="../FE/css/admin/header.css">
+  <link rel="stylesheet" href="../FE/css/admin/reset.css">
+  <link rel="stylesheet" href="../FE/css/admin/statistic.css">
+  <link rel="stylesheet" href="../FE/css/admin/product.css">
+  <link rel="stylesheet" href="../FE/css/admin/order.css">
+  <link rel="stylesheet" href="../FE/css/admin/account.css">
+  <link rel="stylesheet" href="../FE/css/admin/supply.css">
+  <link rel="stylesheet" href="../FE/css/admin/permission.css">
+  <link rel="stylesheet" href="../FE/css/admin/point.css">
+  <link rel="stylesheet" href="../FE/css/admin/discount.css">
+  <!-- js -->
+  <script src="../FE/controller/admin/loadPage.js"></script>
+  <script src="../FE/controller/admin/modalBox.js"></script>
+  <script src="../FE/controller/admin/Manager.js"></script>
+  <title>Administrator</title>
+</head>
+<body > 
+  <div id="warrper">
+    <header id="header">
+      <?php include '../FE/html/admin/header.php' ?>
+    </header>
+    <main >
+      <div id="content">
+      </div>
+</div>
+</main>
+</body>
